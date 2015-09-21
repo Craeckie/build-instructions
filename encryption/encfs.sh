7743b8f015dc037561ddaea3b9d920801faba1aa
josia <josia@example.com>
2015-09-21 17:13:53 +0200

    EncFS: finished

--- encryption/encfs.sh
+++ encryption/encfs.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-apt-get install libfuse-dev libssl-dev libboost-serialization-dev librlog-dev
+# bash fuse.sh
+
+apt-get install libssl-dev libboost-serialization-dev librlog-dev
 
 curl -L https://github.com/vgough/encfs/tarball/master | tar xvz
 
 pushd vgough-encfs-*
+mkdir build
+pushd build
+cmake ..
 make
+make test # optional
+sudo make install
+popd # build
+popd # vgough-encfs-..
+rm -r vgough-encfs-*
+
